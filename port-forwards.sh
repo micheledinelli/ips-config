bd86639480bff974ce731163802cfbde2967b298
micheledinelli <dinellimichele00@gmail.com>
2024-06-04 22:51:40 +0200

    Added port forward to expose backend to the lan

--- port-forwards.sh
+++ port-forwards.sh
@@ -3,3 +3,6 @@ kubectl port-forward svc/backend 8888:8888 & \
 
 # Forward port 80 on the frontend service inside k8s to localhost:8080
 kubectl port-forward svc/frontend 8080:80
+
+# Expose backend outside of localhost (for the mobile application)
+# kubectl port-forward --address=0.0.0.0 deployment/backend 8888:8888 
